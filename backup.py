63471c748153e893bdc636b055a124b0ef325ca4
EC2 Default User <ec2-user@ip-172-31-28-9.ap-southeast-2.compute.internal>
2023-10-31 05:53:17 +0000

    31/10/23

--- backup.py
+++ backup.py
@@ -3,17 +3,19 @@
 import os
 import math
 import sys
-from backupcfg import jobs, backupDir, backupLog,smtp
-from datetime import datetime
+from backupcfg import jobs, backupDir, backupLog, smtp
+from datetime import datetime 
 
 import smtplib
 import pathlib
 import shutil
 
 def errorHandler(errorMessage, dateTimeStamp):
+    print (errorMessage)
+    writeLogMessage(errorMessage, dateTimeStamp, False)
     pass
 
-def writeLogMessagen(logMessage, dateTimeStamp, isSuccess):
+def writeLogMessage(logMessage, dateTimeStamp, isSuccess):
     
     try:
         file = open(backupLog, "a")
@@ -29,41 +31,60 @@ def writeLogMessagen(logMessage, dateTimeStamp, isSuccess):
         print("ERROR: File could not be accessed.")
 
 def main():
+   
     try:
+      
         dateTimeStamp = datetime.now().strftime("%Y%m%d-%H%M%S")
 
-        argCount = len(sys.argv)
+        argCount = len(sys.argv) 
+       
         # Check at least one CLI argument
         if argCount < 2:
             errorHandler("ERROR. Job Not Specified", dateTimeStamp)
         else:
+       
             for jobname in sys.argv[1:]:
             # check for valid job name in directory
                 if not jobname in jobs:
                     errorHandler(f"ERROR. Job {jobname} not on the list", dateTimeStamp)
                 else:
+                    
                     jobsPath=jobs[jobname]
                     #Check source is a valid file/directory
+        
                     if not os.path.exists(jobsPath):
                         errorHandler(f"ERROR: file {jobsPath} does not exist",dateTimeStamp)
                     else:
                         Destination = backupDir
                         # check Destination is valid
                         if not os.path.exists(Destination):
-                        
+                            os.makedirs(jobsPath)
                             errorHandler(f"ERROR: Dir {Destination} does not exist", dateTimeStamp)
                         else:
                             
-                            srcPath = pathlib.purepath(jobsPath)
+                            srcPath = pathlib.PurePath(jobsPath)
                             dstLoc = Destination + "/" + srcPath.name + "-" + dateTimeStamp
                             # copys file/directory to a backup directory
                             if pathlib.Path(jobsPath).is_dir():
                                 shutil.copytree(jobsPath, dstLoc)
                             else:
-                                shutil.copy2(jobsPath, dstLoc)
-                                
+                                shutil.copy(jobsPath, dstLoc)
+                            
+                            writeLogMessage(f"Copied {jobsPath} to {dstLoc}", dateTimeStamp, True) 
+                            print(f"{jobsPath} Has been backed up successfully")
+       # def sendEmail(message):
+            email = 'To: ' + smtp["recipient"] + '\n' + 'From: ' + smtp["sender"] + '\n' + 'Subject: Backup Error\n\n' + message + '\n'
+        
+       #sendEmail(message)
+        smtp_server = smtplib.SMTP(smtp["server"], smtp["port"])
+        smtp_server.ehlo()
+        smtp_server.starttls()
+        smtp_server.ehlo()
+        smtp_server.login(smtp["user"], smtp["password"])
+        smtp_server.sendmail(smtp["sender"], smtp["recipient"], email)
+        smtp_server.close()
     except:
-           print ("ERROR: there was an issue") 
+        print ("ERROR: there was an issue") 
 
 if __name__ == '__main__':
     main()
--- backupcfg.py
+++ backupcfg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-jobs = {"job700" : "1/home/ec2-user/environment/ictprg302/file",
+jobs = {"job700" : "/home/ec2-user/environment/ictprg302/file1",
         "job1" : "/home/ec2-user/environment/ictprg302/dir1"}
 
 
@@ -8,4 +8,9 @@ backupDir = "/home/ec2-user/environment/ictprg302/backups"
 
 backupLog = "/home/ec2-user/environment/ictprg302/backup.log"
 
-smtp = {}
+smtp = {"sender": "dcleary@sunitafe.edu.au",
+        "recipient": "",
+        "server": "smtp.gmail.com",
+        "port": 587,
+        "user": "", # need to specify a gmail email address with an app password setup
+        "password": ""}   # need a gmail app password
